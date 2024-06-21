8b034dfe483d8baed79eee34549b4e541ad25278
david <david.michael.nonn@gmail.com>
2024-06-20 20:13:10 -0600

    Finish up pz server image.

--- apps/project-zomboid/ci/latest.py
+++ apps/project-zomboid/ci/latest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 def get_latest(channel):
-    return "alpine"
+    return "ubuntu"
 
 if __name__ == "__main__":
     import sys
--- apps/project-zomboid/entrypoint.sh
+++ apps/project-zomboid/entrypoint.sh
@@ -4,7 +4,6 @@ set +x -o pipefail
 
 update() {
   echo "[INFO] Updating server"
-  echo "We here: `dirname $0`"
 
   steamcmd +runscript "/app/update_zomboid.txt"
 
@@ -14,11 +13,11 @@ update() {
 start() {
   echo "[INFO] Starting server"
   
-  if [[ -z "$BIND_IP" ]] || [[ "$BIND_IP" == "0.0.0.0" ]]; then
-    BIND_IP=($(hostname -i))
-    BIND_IP=${BIND_IP[0]}
+  if [[ -z "$IP" ]] || [[ "$IP" == "0.0.0.0" ]]; then
+    IP=($(hostname -i))
+    IP=${IP[0]}
   else
-    BIND_IP="$BIND_IP"
+    IP="$IP"
   fi
 
   if [[ -z "$USE_STEAM" ]] || [[ "$USE_STEAM" == "true" ]]; then
@@ -27,18 +26,18 @@ start() {
     USE_STEAM="-nosteam"
   fi
 
-  timeout "$TIMEOUT" /opt/pzserver/start-server.sh \
-    -cachedir=$ZOMBOID_PATH \
-    -Duser.home=$ZOMBOID_PATH \
-    -adminpassword $ADMIN_PASSWORD \
+  timeout $TIMEOUT /app/start-server.sh \
+    -cachedir=/data \
     -ip $IP \
     -port $PORT \
+    -adminpassword $ADMIN_PASSWORD \
     -servername $SERVER_NAME \
     -Xmx${MAX_RAM} \
     -steamvac $STEAM_VAC \
     $USE_STEAM &
 
-  wait $!
+  server_pid=$!
+  wait $server_pid
 
   echo "[INFO] Server stopped"
 }
