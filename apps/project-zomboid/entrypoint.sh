0f7efbc37140ceff334357227fe0b3fe609d2797
david <david.michael.nonn@gmail.com>
2024-06-20 20:34:49 -0600

    Simplify entrypoint and allow args.

--- apps/project-zomboid/entrypoint.sh
+++ apps/project-zomboid/entrypoint.sh
@@ -2,45 +2,28 @@
 
 set +x -o pipefail
 
-update() {
-  echo "[INFO] Updating server"
+steamcmd +runscript "/app/update_zomboid.txt"
 
-  steamcmd +runscript "/app/update_zomboid.txt"
+if [ -z "$IP" ] || [ "$IP" == "0.0.0.0" ]; then
+  IP=($(hostname -i))
+  IP=${IP[0]}
+else
+  IP=$IP
+fi
 
-  echo "[INFO] Server updated"
-}
+if [ -z "$USE_STEAM" ] || [ "$USE_STEAM" == "true" ]; then
+  USE_STEAM=""
+else
+  USE_STEAM="-nosteam"
+fi
 
-start() {
-  echo "[INFO] Starting server"
-  
-  if [[ -z "$IP" ]] || [[ "$IP" == "0.0.0.0" ]]; then
-    IP=($(hostname -i))
-    IP=${IP[0]}
-  else
-    IP="$IP"
-  fi
-
-  if [[ -z "$USE_STEAM" ]] || [[ "$USE_STEAM" == "true" ]]; then
-    USE_STEAM=""
-  else
-    USE_STEAM="-nosteam"
-  fi
-
-  timeout $TIMEOUT /app/start-server.sh \
-    -cachedir=/data \
-    -ip $IP \
-    -port $PORT \
-    -adminpassword $ADMIN_PASSWORD \
-    -servername $SERVER_NAME \
-    -Xmx${MAX_RAM} \
-    -steamvac $STEAM_VAC \
-    $USE_STEAM &
-
-  server_pid=$!
-  wait $server_pid
-
-  echo "[INFO] Server stopped"
-}
-
-update
-start
+exec /app/start-server.sh \
+  -cachedir=/data \
+  -ip $IP \
+  -port $PORT \
+  -adminpassword $ADMIN_PASSWORD \
+  -servername $SERVER_NAME \
+  -Xmx${MAX_RAM} \
+  -steamvac $STEAM_VAC \
+  $USE_STEAM \
+  "$@"
